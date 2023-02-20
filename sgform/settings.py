c69628f9ac535f1d663297f71991da195bcceab7
pawlo-code <paaweel.22@gmailcom>
2023-02-20 12:16:46 +0100

    first commit

--- sgform/settings.py
+++ sgform/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w28vv%+=lkk25la7xz!$n9c8mfm)e^_8agr4s2vbrne$4nco8y'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['146.59.94.232']
 
 
 # Application definition
@@ -34,7 +35,7 @@ INSTALLED_APPS = [
     'sgapp',
     'phone_field',
     'bootstrap5',
-    
+
     #standard
     'django.contrib.admin',
     'django.contrib.auth',
